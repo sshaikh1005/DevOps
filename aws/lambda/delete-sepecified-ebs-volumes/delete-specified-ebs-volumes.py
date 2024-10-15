14440450d5b3e972bf981515469c12dfd6cbdad1
Shaikh <shaikhshahid.ss.141@gmail.com>
2024-10-15 14:49:20 +0530

    DEVOPS PROJECTS

--- aws/lambda/delete-sepecified-ebs-volumes/delete-specified-ebs-volumes.py
+++ aws/lambda/delete-sepecified-ebs-volumes/delete-specified-ebs-volumes.py
@@ -1,6 +1,6 @@
 import boto3
 ec2 = boto3.resource('ec2',region_name='eu-west-3')
-volume_ids = ['vol-029af2107c0a0807d', ‘vol-029af2107c0a08123’]
+volume_ids = ['vol-029af2107c0a0807d', 'vol-029af2107c0a08123']
 def lambda_handler(event, context):
     for volid in volume_ids:
         vid=volid
